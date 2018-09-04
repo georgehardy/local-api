f34e4bdd13aec5d733fd9d07c292b88c0e9070da
georgehardy <george.hardy@gmail.com>
2018-09-04 19:11:40 +1200

    fix typo

--- migrations/20180904165823_roles.js
+++ migrations/20180904165823_roles.js
@@ -1,9 +1,9 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('roles', table => {
     table.increments('id').primary()
-    table.interger('user_id').references('users.id')
-    table.interger('org_id').references('organisations.id')
-    table.interger('role')
+    table.integer('user_id').references('users.id')
+    table.integer('org_id').references('organisations.id')
+    table.integer('role')
     table.timestamps()
   })
 }
