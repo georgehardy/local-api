76a7a8659b4e6132472d7ed08101961140083831
georgehardy <george.hardy@gmail.com>
2018-09-12 10:59:42 +1200

    organisations -> orgs

--- migrations/20180904165823_roles.js
+++ migrations/20180904165823_roles.js
@@ -2,7 +2,7 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable('roles', table => {
     table.increments('id').primary()
     table.integer('user_id').references('users.id')
-    table.integer('org_id').references('organisations.id')
+    table.integer('org_id').references('orgs.id')
     table.integer('role')
     table.timestamps()
   })
