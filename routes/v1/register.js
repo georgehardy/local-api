c6e56c1f3bfa432668884be1bd3142abc3e850c2
georgehardy <george.hardy@gmail.com>
2018-09-12 11:06:14 +1200

    added addOrg route and finalised addUser route

--- routes/v1/register.js
+++ routes/v1/register.js
@@ -5,7 +5,7 @@ const db = require('../../db/register')
 
 const router = express.Router()
 
-function sendError(res, err) {
+function sendError (res, err) {
   console.error('Registration error', err)
   res.status(500).send({error: 'Woops something went wrong...'})
 }
@@ -13,19 +13,28 @@ function sendError(res, err) {
 router.post('/addUser', (req, res) => {
   const user = req.body.user
   !ev.validate(user.email)
-  ? res.status(200).send({error: 'You must enter a valid email address.'})
-  : db.checkEmailExists(user.email)
+    ? res.status(200).send({error: 'You must enter a valid email address.'})
+    : db.checkEmailExists(user.email)
       .then(users => {
         users.length > 0
-        ? res.status(200).send({error: 'This email address has already been used.'})
-        : db.addUser(user)
-          .then(id => {
-            console.log(id)
-            res.status(201).send(id[0])
-          })
-          .catch(err => sendError(res, err))
+          ? res.status(200).send({error: 'This email address has already been used.'})
+          : db.addUser(user)
+            .then(id => {
+              res.status(201).send(id[0])
+            })
+            .catch(err => sendError(res, err))
       })
       .catch(err => sendError(res, err))
+})
+
+router.post('/addOrg', (req, res) => {
+  db.addOrg(req.body.org)
+    .then(id => {
+      db.addRole(req.body.userId, id[0].id, 9)
+        .then(() => res.status(201).send(id[0]))
+        .catch(err => sendError(res, err))
+    })
+    .catch(err => sendError(res, err))
 })
 
 router.get('/:id', (req, res) => {
